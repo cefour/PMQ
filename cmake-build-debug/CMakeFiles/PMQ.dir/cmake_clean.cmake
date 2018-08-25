d0c05dca326a0e9b832c7e634d015fc36e5857e0
Michael Burzan <Michael.Burzan@outlook.de>
2018-08-25 10:40:44 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/PMQ.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PMQ.dir/main.cpp.o"
+  "PMQ.pdb"
+  "PMQ"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PMQ.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/PMQ.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PMQ.
+# This may be replaced when dependencies are built.
